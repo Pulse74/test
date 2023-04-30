4243359d8759d4240f4c911a12c585184a0d4c7c
Pulse74 <alexamattos@gmail.com>
2023-04-30 12:44:39 -0300

    ....

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace Variables
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("{0} {1} {2}", byte.MinValue, byte.MaxValue, "Valores de byte");
         }
     }
 }
